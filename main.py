141ba65bc0b1e922d6f2837f2c3dfe5679a06cbc
Yuval Schwartz <yuvsc@post.bgu.ac.il>
2024-02-27 18:26:09 +0200

    Initial Implementation of the method.

--- main.py
+++ main.py
@@ -12,16 +12,24 @@ def load_data():
     por_df = pd.read_csv(por_data_path)
     por_df['course'] = 'portuguese'
     students_df = pd.concat([mat_df, por_df])
+    cat_columns = students_df.select_dtypes(['object']).columns
+    students_df[cat_columns] = students_df[cat_columns].astype('category')
     return students_df
 
 
 def main():
+    seed = 56
     students_df = load_data()
     x = students_df.drop(['G3'], axis=1)
     y = students_df['G3']
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-    model = xgb.XGBClassifier()
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=seed)
+    model = xgb.XGBRegressor(objective='reg:squarederror', enable_categorical=True, random_state=seed)
     model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    results = pd.DataFrame({'y_test': y_test, 'y_pred': y_pred})
+    results.to_csv('results.csv')
+    mae = (abs(y_test - y_pred)).mean()
+    print(f'Mean Absolute Error: {mae}')
 
 
 if __name__ == '__main__':
