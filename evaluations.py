removing CV from main + adjusting run_regressor and find_optimal_params + refining evaluation module + fixing CI calculation bug + adding maes histogram plot function

evaluations.py
@@ -3,62 +3,88 @@ import numpy as np
 import scipy.stats as stats
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib.patches import Patch
 from sklearn.metrics import mean_absolute_error
 
 class Evaluation:
-    def __init__(self):
-        alpha = 0.05
+    def __init__(self, results_file_path="results.csv"):
+        self.results_df = pd.read_csv(results_file_path)
+        self.alpha = 0.05
+        self.number_of_bootstrap_samples = 1000
 
-    def calculateMAE(self, df) -> float:
-        return (abs(df['y_test'] - df['y_pred'])).mean()
-        
+    def calculateMAE(self) -> float:
+        return (abs(self.results_df['y_test'] - self.results_df['y_pred'])).mean()
 
-    def calaculateMAE_WithRounding(self, df) -> float:
-        return (abs(df['y_test'] - df['y_pred'].round())).mean()
-    
-    def calculateStandardDeviation(self, df) -> float:
-        return (abs(df['y_test'] - df['y_pred'])).std()
-    
-    def confidenceItervalTest(self, maeBaseline, maeModified=0.801, standartDeviation=1, alpha=0.05) -> bool:
-        z_score = np.quantile(np.random.normal(0, 1, 10000), 1 - alpha/2)
+    def calaculateMAE_WithRounding(self) -> float:
+        return (abs(self.results_df['y_test'] - self.results_df['y_pred'].round())).mean()
+
+    def calculateStandardDeviation(self) -> float:
+        return (abs(self.results_df['y_test'] - self.results_df['y_pred'])).std()
+
+    def confidenceItervalTest(self, maeBaseline, maeModified=0.801, standartDeviation=1) -> bool:
+        z_score = np.quantile(np.random.normal(0, 1, 10000), 1 - self.alpha / 2)
         print(f'Z-score: {z_score}')
         
         ci_lower = maeModified - z_score * standartDeviation
         ci_upper = maeModified + z_score * standartDeviation
         
         if ci_lower <= maeBaseline <= ci_upper:
-            print(f"The baseline score ({maeBaseline}) falls within the confidence interval of the modified model ({ci_lower:.4f} , {ci_upper:.4f}) at {alpha*100:.0f}% confidence level.")
+            print(f"The baseline score ({maeBaseline}) falls within the confidence interval of the modified model ({ci_lower:.4f} , {ci_upper:.4f}) at {self.alpha * 100:.0f}% confidence level.")
             print(f"The difference is not significant.")
             return False
         else:
-            print(f"The baseline score ({maeBaseline}) falls outside the confidence interval of the modified model ({ci_lower:.4f} , {ci_upper:.4f}) at {alpha*100:.0f}% confidence level.")
+            print(f"The baseline score ({maeBaseline}) falls outside the confidence interval of the modified model ({ci_lower:.4f} , {ci_upper:.4f}) at {self.alpha * 100:.0f}% confidence level.")
             print(f"The difference is significant.")
             return True
 
-    def bootstrap_hypothesis_test(alpha_value=0.05, dataset_file="results.csv", num_bootstrap=1000, reference_mae=0.801):
-        
-        dataset = pd.read_csv(dataset_file)
-        original_mae = reference_mae
-
+    def bootstrap_hypothesis_test(self, reference_mae=0.801):
         bootstrap_mae_scores = []
-        for _ in range(num_bootstrap):
-            bootstrap_sample = dataset.sample(frac=1, replace=True)
+        for _ in range(self.number_of_bootstrap_samples):
+            bootstrap_sample = self.results_df.sample(frac=1, replace=True)
             mae = mean_absolute_error(bootstrap_sample['y_test'], bootstrap_sample['y_pred'])
             bootstrap_mae_scores.append(mae)
-        
-        lower_percentile = (1 - 0.05) / 2 * 100
+
+        lower_percentile = self.alpha / 2 * 100
         upper_percentile = 100 - lower_percentile
         lower_bound = np.percentile(bootstrap_mae_scores, lower_percentile)
         upper_bound = np.percentile(bootstrap_mae_scores, upper_percentile)
+
+        # Print confidence interval
+        print(f"MAE = {np.mean(bootstrap_mae_scores):.3f}, {(1 - self.alpha) * 100:g}% CI: {lower_bound:.3f}-{upper_bound:.3f} (based on {self.number_of_bootstrap_samples} bootstraps).")
         
         # Hypothesis testing
-        if original_mae < lower_bound:
+        if reference_mae < lower_bound:
             print("The original model performs significantly better than the new model.")
-        elif original_mae > upper_bound:
+        elif reference_mae > upper_bound:
             print("The new model performs significantly better than the original model.")
         else:
             print("There is no significant difference between the performance of the original and new models.")
-        
-        # Print confidence interval
-        print("Bootstrap confidence interval ({}%): ({:.4f}, {:.4f})".format((1-0.05)*100, lower_bound, upper_bound))
-        
+
+    def plot_maes_histogram(self):
+        bootstrap_mae_scores = []
+        for _ in range(self.number_of_bootstrap_samples):
+            bootstrap_sample = self.results_df.sample(frac=1, replace=True)
+            mae = mean_absolute_error(bootstrap_sample['y_test'], bootstrap_sample['y_pred'])
+            bootstrap_mae_scores.append(mae)
+
+        lower_percentile = self.alpha / 2 * 100
+        upper_percentile = 100 - lower_percentile
+        lower_bound = np.percentile(bootstrap_mae_scores, lower_percentile)
+        upper_bound = np.percentile(bootstrap_mae_scores, upper_percentile)
+
+        fig_hist, ax_hist = plt.subplots()
+        ax_hist.hist(bootstrap_mae_scores, bins=12, color='lightsteelblue', ec="k")
+        ax_hist.set_title(f'Distribution of MAEs Using {self.number_of_bootstrap_samples} Bootstraps')
+        ax_hist.set_xlabel('MAEs')
+        ax_hist.set_ylabel('Frequency')
+        fig_hist.subplots_adjust(bottom=0.18, left=0.18)
+        ax_hist.axvline(x=lower_bound, color='dimgrey', linestyle='--', linewidth=1.25)
+        ax_hist.axvline(x=upper_bound, color='dimgrey', linestyle='--', linewidth=1.25)
+        legend_patch = Patch(color='lightsteelblue', label=f"XGBoost (MAE = {np.mean(bootstrap_mae_scores):.3f}, {(1 - self.alpha) * 100:g}% CI: {lower_bound:.3f}-{upper_bound:.3f})")
+        ax_hist.legend(handles=[legend_patch], loc='upper center', bbox_to_anchor=(0.5, -0.15))
+
+        # Save the histogram image
+        histogram_image_name = f"maes_histogram.png"
+        plt.savefig(histogram_image_name, dpi=300)
+        plt.close(fig_hist)

main.py
@@ -7,7 +7,6 @@ import xgboost as xgb
 import optuna
 from xgboost import plot_importance
 import matplotlib.pyplot as plt
-
 from evaluations import Evaluation
 
 
@@ -22,7 +21,11 @@ def load_data():
     return students_df
 
 
-def feature_engineering(X):
+def feature_selection(X):
+    return X
+
+
+def feature_extraction(X):
     X['G1_G2_interaction'] = np.abs(X['G1'] - X['G2'])
     return X
 
@@ -35,7 +38,6 @@ def convert_object_to_category(X):
 
 def find_optimal_params(X, y, seed):
     def objective(trial):
-        x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
         params = {
             'max_depth': trial.suggest_int('max_depth', 1, 6),
             'learning_rate': trial.suggest_float('learning_rate', 0.01, 1.0),
@@ -48,12 +50,8 @@ def find_optimal_params(X, y, seed):
             'reg_lambda': trial.suggest_float('reg_lambda', 0.01, 1.0),
             'random_state': trial.suggest_int('random_state', 1, 1000)
         }
-        model = xgb.XGBRegressor(**params, enable_categorical=True)
-        model.fit(x_train, y_train)
-        y_pred = model.predict(x_test)
-        y_pred = np.round(y_pred)
-        y_pred = np.clip(y_pred, 0, 20)
-        mae = mean_absolute_error(y_test, y_pred)
+        model_results = run_regressor(X, y, seed, params)
+        mae = mean_absolute_error(model_results['y_test'], model_results['y_pred'])
         return mae
     study = optuna.create_study(direction='minimize')
     study.optimize(objective, n_trials=3000)
@@ -68,56 +66,51 @@ def find_optimal_params(X, y, seed):
 
 
 def run_regressor(X, y, seed, optimal_params=None):
-    n_splits = 10
-    kf = KFold(n_splits=n_splits, shuffle=True, random_state=seed)
-    mae_list = []
-    for train_index, test_index in kf.split(X):
-        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-        if optimal_params is None:
-            model = xgb.XGBRegressor(enable_categorical=True)
-        else:
-            model = xgb.XGBRegressor(**optimal_params, enable_categorical=True)
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
-        y_pred = np.round(y_pred)
-        y_pred = np.clip(y_pred, 0, 20)
-        mae = mean_absolute_error(y_test, y_pred)
-        mae_list.append(mae)
-    avg_mae = np.mean(mae_list)
-    print(f'Average Mean Absolute Error across {n_splits} folds: {avg_mae}')
-    importance = model.get_booster().get_score(importance_type='weight')
-    print(importance)
-    fig, ax = plt.subplots(figsize=(10, 8))
-    plot_importance(model, ax=ax, importance_type='weight')
-    plt.show()
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
+    if optimal_params is None:
+        model = xgb.XGBRegressor(enable_categorical=True)
+    else:
+        model = xgb.XGBRegressor(enable_categorical=True, **optimal_params)
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    y_pred = np.round(y_pred)
+    y_pred = np.clip(y_pred, 0, 20)
+    # importance = model.get_booster().get_score(importance_type='weight')
+    # print(importance)
+    # fig, ax = plt.subplots(figsize=(10, 8))
+    # plot_importance(model, ax=ax, importance_type='weight')
+    # plt.show()
+    results = pd.DataFrame({'y_test': y_test, 'y_pred': y_pred})
+    return results
 
 
 def main():
     seed = 42
     students_df = load_data()
     X = students_df.drop(['G3'], axis=1)
-    X = feature_engineering(X)
+    X = feature_selection(X)
+    X = feature_extraction(X)
     X = convert_object_to_category(X)
     y = students_df['G3']
     # optimal_params = find_optimal_params(X, y, seed)
-    optimal_params = {
-        'max_depth': 3,
-        'learning_rate': 0.010322048435422228,
-        'n_estimators': 384,
-        'min_child_weight': 19,
-        'gamma': 0.9272196990826382,
-        'subsample': 0.918293434562889,
-        'colsample_bytree': 0.9986801767250817,
-        'reg_alpha': 0.4746275842672103,
-        'reg_lambda': 0.7525318986291032,
-        'random_state': 908
-    }
+    # optimal_params = {
+    #     'max_depth': 3,
+    #     'learning_rate': 0.010322048435422228,
+    #     'n_estimators': 384,
+    #     'min_child_weight': 19,
+    #     'gamma': 0.9272196990826382,
+    #     'subsample': 0.918293434562889,
+    #     'colsample_bytree': 0.9986801767250817,
+    #     'reg_alpha': 0.4746275842672103,
+    #     'reg_lambda': 0.7525318986291032,
+    #     'random_state': 908
+    # }
     # run_regressor(X, y, seed, optimal_params)
-    # run_regressor(X, y, seed)
-
+    model_results = run_regressor(X, y, seed)
+    model_results.to_csv('results.csv')
     evaluation = Evaluation()
     evaluation.bootstrap_hypothesis_test()
+    evaluation.plot_maes_histogram()
 
 if __name__ == '__main__':
     main()